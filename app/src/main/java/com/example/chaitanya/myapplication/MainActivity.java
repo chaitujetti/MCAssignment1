b41ad0c69e704d7b5cf65f63bd449dee7efb2720
chaitanya jetti <chaitanya@chaitanyas-mbp.mobile.asu.edu>
2017-01-31 18:36:00 -0700

    Moving Graph working perfectly on multiple run and stop button presses.

--- app/src/main/java/com/example/chaitanya/myapplication/MainActivity.java
+++ app/src/main/java/com/example/chaitanya/myapplication/MainActivity.java
@@ -1,18 +1,69 @@
 package com.example.chaitanya.myapplication;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    GraphView g;
+    LinearLayout graph;
+    float[] values;
+    Thread movingGraph = null;
+    Boolean flag = null;
+
+    Handler threadHandle = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            for (int i = 0; i < 10; i++) {
+                values[i] = (float) Math.ceil(Math.random() * 180);
+            }
+            g.invalidate();
+            g.setValues(values);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+
+        Button buttonRun= (Button)findViewById(R.id.buttonRun);
+        buttonRun.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                if(flag == null){
+                    flag = true;
+                    movingGraph.start();
+                    Toast.makeText(MainActivity.this, "Graph Started", Toast.LENGTH_SHORT).show();
+                } else if(!flag){
+                    flag = true;
+                    Toast.makeText(MainActivity.this, "Graph Resumed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Button buttonStop= (Button)findViewById(R.id.buttonStop);
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                flag = false;
+                Toast.makeText(MainActivity.this, "Graph Stopped", Toast.LENGTH_SHORT).show();
+                for (int i = 0; i < 10; i++) {
+                    values[i] = 0;
+                }
+                g.invalidate();
+                g.setValues(values);
+            }
+        });
+    }
+
+    protected void init(){
         String[] hlabels= new String[10];
         for (int i = 0; i < 10; i++) {
             hlabels[i]=String.valueOf(i * 18);
@@ -25,51 +76,29 @@ public class MainActivity extends AppCompatActivity {
             vlabels[i]=String.valueOf((i * 10) + k);
         }
 
-        final float[] values= new float[10];
-        final GraphView g = new GraphView(MainActivity.this,values,"Main Graph",hlabels,vlabels,GraphView.LINE);
-        LinearLayout graph= (LinearLayout)findViewById(R.id.graphll);
+        values= new float[10];
+        g = new GraphView(MainActivity.this, values, "Main Graph", vlabels, hlabels, GraphView.LINE);
+        graph= (LinearLayout)findViewById(R.id.graphll);
         graph.addView(g);
 
-        Thread movingGraph = new Thread(new Runnable() {
+        movingGraph = new Thread(new Runnable() {
             @Override
             public void run() {
-
-            }
-        });
-
-        Button buttonRun= (Button)findViewById(R.id.buttonRun);
-        buttonRun.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                int k = 0;
-                while(k < 10) {
-                    for (int i = 0; i < 10; i++) {
-                          values[i] = (float) Math.random();
-                        values[i] = (float) Math.ceil(Math.random() * 180);
+                try {
+                    int k = 0;
+                    while(true){
+                        while(flag) {
+                            k++;
+                            Message msg = threadHandle.obtainMessage(1,Integer.toString(k));
+                            threadHandle.sendMessage(msg);
+                            Thread.sleep(100);
+                        }
                     }
-                    g.invalidate();
-                    g.setValues(values);
-                    try {
-                        Thread.sleep(1);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    k++;
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         });
-
-        Button buttonStop= (Button)findViewById(R.id.buttonStop);
-        buttonStop.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Thread.currentThread().interrupt();
-                for (int i = 0; i < 10; i++) {
-                    values[i] = 0;
-                }
-                g.invalidate();
-                g.setValues(values);
-            }
-        });
-
-
     }
 }
