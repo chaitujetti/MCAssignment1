838ee2a0613d584c21109cdfaff2daa3d83d5986
chaitanya jetti <chaitanya@chaitanyas-MacBook-Pro.local>
2017-02-01 00:46:02 -0700

    Added all required components to the GUI in the correct order and format.

--- app/src/main/java/com/example/chaitanya/myapplication/MainActivity.java
+++ app/src/main/java/com/example/chaitanya/myapplication/MainActivity.java
@@ -101,4 +101,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+//    public void onRadioButtonClicked(View view) {
+//        // Is the button now checked?
+//        boolean checked = ((RadioButton) view).isChecked();
+//
+//        // Check which radio button was clicked
+//        switch(view.getId()) {
+//            case R.id.radio_pirates:
+//                if (checked)
+//                    // Pirates are the best
+//                    break;
+//            case R.id.radio_ninjas:
+//                if (checked)
+//                    // Ninjas rule
+//                    break;
+//        }
+//    }
 }
